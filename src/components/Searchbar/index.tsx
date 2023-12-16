90988b71d6fc091c11cfe898585f5daea5a0852a
Joeumar Souza <joeu@Joeumars-MacBook-Pro.local>
2023-12-16 15:24:58 -0300

    feat: creating searchbar component

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSearch = (): void => {
+    console.log("run search with: " + inputValue);
+    setInputValue("");
+  };
+
+  return (
+    <div className="container">
+      <input type="text" onChange={handleChange} value={inputValue} />
+      <span className="container__icon" onClick={handleSearch}>
+        <AiOutlineSearch color="gray" size={24} />
+      </span>
+    </div>
+  );
 };
 
 export default SearchBar;
