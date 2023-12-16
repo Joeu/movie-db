cd538ec284a74f2ff978984fb0d6809d183617f4
Joeumar Souza <joeu@Joeumars-MacBook-Pro.local>
2023-12-16 15:52:31 -0300

    feat: handling current route

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const NavMenu: React.FC = () => (
-  <nav>
-    {["Home", "WhatchList"].map((item, idx) => (
-      <Link key={idx} to={item.toLowerCase()}>
-        {item}
-      </Link>
-    ))}
-  </nav>
-);
+const NavMenu: React.FC = () => {
+  const location = useLocation();
+  const currentLocation = location.pathname;
+
+  return (
+    <nav>
+      {["Home", "WhatchList"].map((item, idx) => (
+        <Link
+          key={idx}
+          to={item.toLowerCase()}
+          className={
+            currentLocation === "/" + item.toLocaleLowerCase() ? "selected" : ""
+          }
+        >
+          {item}
+        </Link>
+      ))}
+    </nav>
+  );
+};
 
 export default NavMenu;
