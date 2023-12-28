dbb3abf1fef03ebc72f08ad5fb73974bee5d360c
Joeumar Souza <joeu@Joeumars-MacBook-Pro.local>
2023-12-28 09:18:07 -0300

    feat: lazy loading routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Home from "../pages/Home";
 import WatchList from "../pages/WatchList";
-import { QueryClient, QueryClientProvider } from "react-query";
-import Results from "../pages/Results";
-import DetailsPage from "../pages/Details";
 
 const queryClient = new QueryClient();
 
+const Results = React.lazy(() => import("../pages/Results"));
+const DetailsPage = React.lazy(() => import("../pages/Details"));
+
 const AppRoutes: React.FC = () => (
   <QueryClientProvider client={queryClient}>
-    <Routes>
-      <Route path="/home" Component={Home} />
-      <Route path="/watchlist" Component={WatchList} />
-      <Route path="/results" Component={Results} />
-      <Route path="/movies/:movieId" Component={DetailsPage} />
-      <Route path="/*" Component={Home} />
-    </Routes>
+    <Suspense fallback={<div>loading...</div>}>
+      <Routes>
+        <Route path="/home" Component={Home} />
+        <Route path="/watchlist" Component={WatchList} />
+        <Route path="/results" Component={Results} />
+        <Route path="/movies/:movieId" Component={DetailsPage} />
+        <Route path="/*" Component={Home} />
+      </Routes>
+    </Suspense>
   </QueryClientProvider>
 );
 
